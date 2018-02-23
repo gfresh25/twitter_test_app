f79edbe9790c48cd99b2bcd58780f9ee5ac96cdc
Garrett Freshwater <garrettfreshwater@gmail.com>
2018-02-22 22:11:06 -0600

    fixed error in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+def top_tweeter
+	tweets = Tweet.all
+	if tweets.length > 0
+		user_id = Tweet.all.group_by { |h| h['user_id']}.to_a.max_by {|x| x[1].length}.first
+		user = User.find(user_id)
+		return user
+	end
+end
 
 end
 
